2f618f5ceadef4fe55f89325ca243bd60dbf3800
Cleophus Alves <cleophus775@gmail.com>
2025-01-11 07:02:32 +0530

    changed name

--- whack-a-pig/aimgame.py
+++ whack-a-pig/aimgame.py
@@ -6,7 +6,7 @@ import pygame
 pygame.init()
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Aim Trainer")
+pygame.display.set_caption("Whack A Pig")
 
 TARGET_INCREMENT = 400
 TARGET_EVENT = pygame.USEREVENT
